136dc62cb852a63362da2ecdb11b0ed3ed03bd58
Edi <eduard.lupu@bitstone.eu>
2023-08-14 12:58:46 +0300

    Pushed with HashRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.css'
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Shop from "./components/shop";
 import Cart from "./components/cart";
