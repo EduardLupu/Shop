f238174187f77153c449373dc3c785398151fb40
EduardLupu <lupu.eduard.adrian@gmail.com>
2023-09-04 09:54:41 +0300

    Changed from HashRouter to BrowserRouter.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './index.css'
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Shop from "./components/shop";
 import Cart from "./components/cart";
