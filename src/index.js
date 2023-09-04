8e32bb485af678809e072b359ef51a1cd8a0b0fd
EduardLupu <lupu.eduard.adrian@gmail.com>
2023-09-04 13:16:11 +0300

    Added disable-react-devtools

--- src/index.js
+++ src/index.js
@@ -5,8 +5,13 @@ import reportWebVitals from './reportWebVitals';
 import App from './App';
 import {Provider} from "react-redux";
 import store from "./app/store";
+import {disableReactDevTools} from "@fvilers/disable-react-devtools";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+if (process.env.NODE_ENV === 'production') {
+    disableReactDevTools();
+}
 
 root.render(
   <React.StrictMode>
