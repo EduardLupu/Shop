1e7fa82b28c4ad65ecf70a6ec546bcae5d5ec3c7
EduardLupu <lupu.eduard.adrian@gmail.com>
2023-09-09 15:50:54 +0300

    Added new baseUrl.

--- src/app/apiSlice.js
+++ src/app/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        baseUrl: "https://localhost:8080/api",
+        baseUrl: "https://eal.social/api",
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('token');
             if (token) {
